1419d68f33ea8b9a8ec5bed6f58935ddf93d8123
Nicolas Ferrero <ferrero.nicolas@gmail.com>
2013-10-02 17:28:25 +0200

    fix: fix colorized output

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-'use strict';
+var BaseColorReporter = require('../karma/lib/reporters/BaseColor');
 
-
-var MinimalistReporter = function(baseReporterDecorator, color) {
+var MinimalistReporter = function(baseReporterDecorator, colors) {
 
     baseReporterDecorator(this);
 
-    if (color) {
-        this.USE_COLORS = true;
-
-        this.LOG_SINGLE_BROWSER = '%s: ' + '%s'.cyan + '\n';
-        this.LOG_MULTI_BROWSER = '%s %s: ' + '%s'.cyan + '\n';
-
-        this.SPEC_FAILURE = '%s %s FAILED'.red + '\n';
-        this.SPEC_SLOW = '%s SLOW %s: %s'.yellow + '\n';
-        this.ERROR = '%s ERROR'.red + '\n';
-
-        this.FINISHED_ERROR = ' ERROR'.red;
-        this.FINISHED_SUCCESS = ' SUCCESS'.green;
-        this.FINISHED_DISCONNECTED = ' DISCONNECTED'.red;
-
-        this.X_FAILED = ' (%d FAILED)'.red;
-
-        this.TOTAL_SUCCESS = 'TOTAL: %d SUCCESS'.green + '\n';
-        this.TOTAL_FAILED = 'TOTAL: %d FAILED, %d SUCCESS'.red + '\n';
+    if (colors) {
+        BaseColorReporter.call(this);
     }
 
     this.onBrowserComplete = function(browser) {
@@ -41,7 +24,7 @@ var MinimalistReporter = function(baseReporterDecorator, color) {
 
 };
 
-MinimalistReporter.$inject = ['baseReporterDecorator', 'config.color'];
+MinimalistReporter.$inject = ['baseReporterDecorator', 'config.colors'];
 
 // PUBLISH DI MODULE
 module.exports = {
